5fcadd90e0dfab77cad6dc43194c410f28318f7c
ToM <tomtom637@gmail.com>
2017-10-19 17:43:17 +0200

    finished installing babel with a headache

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,7 +8,7 @@ gulp.task('watch', function() {
 		server: {
 			baseDir: "app"
 		}
-	})
+	});
 	
 	watch('./app/index.html', function() {
 		browserSync.reload();
@@ -16,11 +16,18 @@ gulp.task('watch', function() {
 
 	watch('./app/assets/styles/**/*.css', function() {
 		gulp.start('cssInject');
+	});
 
-	})
+	watch('./app/assets/scripts/**/*.js', function() {
+		gulp.start('scriptsRefresh');
+	});
 });
 
 gulp.task('cssInject' , ['styles'], function(){
 	return gulp.src('./app/temp/styles/styles.css')
 	.pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+	browserSync.reload();
 });
