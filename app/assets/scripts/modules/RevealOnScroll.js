554588f4d8d10ce7de2e308e86c9e40b0d615fa9
ToM <tomtom637@gmail.com>
2017-10-23 00:01:05 +0200

    finished the reveal on scroll part

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -2,10 +2,11 @@ import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll {
-	constructor() {
-		this.itemsToReveal = $(".feature-item");
+	constructor(els, offset) {
+		this.itemsToReveal = els;
+		this.offsetPercentage = offset;
 		this.hideInitially();
-		this.createWaypoints();
+		this.createWaypoints();		
 	}
 
 	hideInitially() {
@@ -13,10 +14,15 @@ class RevealOnScroll {
 	}
 
 	createWaypoints() {
+		var that = this
 		this.itemsToReveal.each(function() {
-			new waypoints({
-				element: x,
-				handler: x,
+			var currentItem = this;
+			new Waypoint({
+				element: currentItem,
+				handler: function() {
+					$(currentItem).addClass("reveal-item--is-visible");
+				},
+				offset: that.offsetPercentage
 			});
 		});
 	}
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -10330,6 +10330,10 @@ return jQuery;
 "use strict";
 
 
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
 var _MobileMenu = __webpack_require__(2);
 
 var _MobileMenu2 = _interopRequireDefault(_MobileMenu);
@@ -10341,7 +10345,8 @@ var _RevealOnScroll2 = _interopRequireDefault(_RevealOnScroll);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var mobileMenu = new _MobileMenu2.default();
-var revealOnScroll = new _RevealOnScroll2.default();
+new _RevealOnScroll2.default((0, _jquery2.default)(".feature-item"), "85%");
+new _RevealOnScroll2.default((0, _jquery2.default)(".testimonial"), "60%");
 
 /***/ }),
 /* 2 */
@@ -10419,10 +10424,11 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var RevealOnScroll = function () {
-	function RevealOnScroll() {
+	function RevealOnScroll(els, offset) {
 		_classCallCheck(this, RevealOnScroll);
 
-		this.itemsToReveal = (0, _jquery2.default)(".feature-item");
+		this.itemsToReveal = els;
+		this.offsetPercentage = offset;
 		this.hideInitially();
 		this.createWaypoints();
 	}
@@ -10435,10 +10441,15 @@ var RevealOnScroll = function () {
 	}, {
 		key: 'createWaypoints',
 		value: function createWaypoints() {
+			var that = this;
 			this.itemsToReveal.each(function () {
-				new _noframework2.default({
-					element: x,
-					handler: x
+				var currentItem = this;
+				new Waypoint({
+					element: currentItem,
+					handler: function handler() {
+						(0, _jquery2.default)(currentItem).addClass("reveal-item--is-visible");
+					},
+					offset: that.offsetPercentage
 				});
 			});
 		}
